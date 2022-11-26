a61bb546011699c4582ffe9403764071eddc40d2
rashikapr <74289013+rashikapr@users.noreply.github.com>
2022-11-26 14:05:47 +0530

    Update scraper_2.py

--- scraper_2.py
+++ scraper_2.py
@@ -68,7 +68,43 @@ scrape()
 new_planets_data = []
 
 def scrape_more_data(hyperlink):
-    
+    try:
+        page = requests.get(hyperlink)
+      
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        temp_list = []
+
+        for tr_tag in soup.find_all("tr", attrs={"class": "fact_row"}):
+            td_tags = tr_tag.find_all("td")
+          
+            for td_tag in td_tags:
+                try: 
+                    temp_list.append(td_tag.find_all("div", attrs={"class": "value"})[0].contents[0])
+                except:
+                    temp_list.append("")
+                    
+        new_planets_data.append(temp_list)
+
+    except:
+        time.sleep(1)
+        scrape_more_data(hyperlink)
+
+#Calling method
+
+for index, data in enumerate(planets_data):
+    scrape_more_data(data[5])
+    print(f"scraping at hyperlink {index+1} is completed.")
+
+print(new_planets_data[0:10])
+
+final_planet_data = []
+
+for index, data in enumerate(planets_data):
+    new_planet_data_element = new_planets_data[index]
+    new_planet_data_element = [elem.replace("\n", "") for elem in new_planet_data_element]
+    new_planet_data_element = new_planet_data_element[:7]
+    final_planet_data.append(data + new_planet_data_element)
 
 with open("final.csv", "w") as f:
         csvwriter = csv.writer(f)
